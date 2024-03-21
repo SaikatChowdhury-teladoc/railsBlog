b46c85eb0328d6abf4aebd2e57b4e0c544530a6b
SaikatChowdhury-teladoc <saikat.chowdhury@teladochealth.com>
2024-03-21 08:27:22 +0530

    graphQL with argument is implemented

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,14 @@
 module Types
   class QueryType < Types::BaseObject
     field :articles, [ArticleType], null: false
-
+    field :article, ArticleType, null: true do
+      argument :id, ID, required:true
+    end
     def articles
-      Article.all
+      Article.first
+    end
+    def article(id:)
+      Article.find_by(id: id)
     end
   end
 end
